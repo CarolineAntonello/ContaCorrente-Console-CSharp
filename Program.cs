abae8e7807d7979a2e9775b2bd99cea3a7c677ff
Caroline Antonello <caroles.antonello@gmail.com>
2018-03-06 22:22:02 -0300

    Orientação a Objetos - Opção sacar PRONTA

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace ContaCorrente
             contacorrente.numero_conta = 1556;
             contacorrente.status = "Especial";
             contacorrente.Deposita(500.0);
+            contacorrente.Saque(1800.0);
 
             Console.ReadKey();
         }
@@ -28,16 +29,40 @@ namespace ContaCorrente
             public int numero_conta;
             public String status;
 
+            //DEPOSITAR 
             public void Deposita(double valor)
             {
                 Console.WriteLine("Saldo Anterior: {0}", this.saldo);
 
                 this.saldo += valor;
 
+                Console.WriteLine("\nDepositado R$ {0}", valor);
+
                 Console.Write("\nSaldo atual: {0}\n", this.saldo);
 
             }
 
+            //SACAR
+            public bool Saque(double valor)
+            {
+                double total;
+                total = this.saldo + this.limite;
+
+                if (total > valor)
+                {
+                    this.saldo -= valor;
+                    Console.Write("\nSaque realizado no valor de: {0}\n", valor);
+                    Console.Write("\nSaldo após o saque: {0}\n", this.saldo);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("\nQuantia a ser sacada: R$ {0}", valor);
+                    Console.Write("\nSaldo insuficiente para saque! \n");
+                    return false;
+                }
+
+            }
 
         }
 
