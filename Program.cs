e9eb6607d21d6a2cfa79cdd372efa31fd41a7b5e
Caroline Antonello <caroles.antonello@gmail.com>
2018-03-06 22:13:34 -0300

    Orientação a Objetos - Opção depositar PRONTA

--- Program.cs
+++ Program.cs
@@ -10,6 +10,37 @@ namespace ContaCorrente
     {
         static void Main(string[] args)
         {
+            ContaCorrente contacorrente = new ContaCorrente();
+
+            contacorrente.saldo = 1000;
+            contacorrente.limite = 100;
+            contacorrente.numero_conta = 1556;
+            contacorrente.status = "Especial";
+            contacorrente.Deposita(500.0);
+
+            Console.ReadKey();
         }
+
+        public class ContaCorrente
+        {
+            public double saldo;
+            public double limite;
+            public int numero_conta;
+            public String status;
+
+            public void Deposita(double valor)
+            {
+                Console.WriteLine("Saldo Anterior: {0}", this.saldo);
+
+                this.saldo += valor;
+
+                Console.Write("\nSaldo atual: {0}\n", this.saldo);
+
+            }
+
+
+        }
+
     }
 }
+
