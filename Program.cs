1369c5a958cc2d24ec950ba3c085069bd6ebfe82
Caroline Antonello <caroles.antonello@gmail.com>
2018-03-06 22:34:28 -0300

    Orientação a Objetos - Opção transferência PRONTA

--- Program.cs
+++ Program.cs
@@ -19,6 +19,14 @@ namespace ContaCorrente
             contacorrente.Deposita(500.0);
             contacorrente.Saque(1800.0);
 
+            ContaCorrente conta_1 = new ContaCorrente();
+            conta_1.saldo = 2000;
+
+            ContaCorrente conta_2 = new ContaCorrente();
+            conta_2.saldo = 1100;
+
+            conta_1.Transferencia(500.0, conta_1, conta_2);
+
             Console.ReadKey();
         }
 
@@ -32,13 +40,14 @@ namespace ContaCorrente
             //DEPOSITAR 
             public void Deposita(double valor)
             {
-                Console.WriteLine("Saldo Anterior: {0}", this.saldo);
+                Console.WriteLine("\n-----DEPOSITO-----");
+                Console.WriteLine("\nSaldo Anterior: {0}", this.saldo);
 
                 this.saldo += valor;
 
                 Console.WriteLine("\nDepositado R$ {0}", valor);
 
-                Console.Write("\nSaldo atual: {0}\n", this.saldo);
+                Console.WriteLine("\nSaldo atual: {0}\n", this.saldo);
 
             }
 
@@ -51,16 +60,36 @@ namespace ContaCorrente
                 if (total > valor)
                 {
                     this.saldo -= valor;
-                    Console.Write("\nSaque realizado no valor de: {0}\n", valor);
-                    Console.Write("\nSaldo após o saque: {0}\n", this.saldo);
+                    Console.WriteLine("\n-----SAQUE-----");
+                    Console.WriteLine("\nSaque realizado no valor de: {0}\n", valor);
+                    Console.WriteLine("\nSaldo após o saque: {0}\n", this.saldo);
                     return true;
                 }
                 else
                 {
-                    Console.WriteLine("\nQuantia a ser sacada: R$ {0}", valor);
-                    Console.Write("\nSaldo insuficiente para saque! \n");
+                    Console.WriteLine("\n-----SAQUE-----");
+                    Console.WriteLine("\nQuantia a ser sacada: R${0}", valor);
+                    Console.WriteLine("\nSaldo insuficiente para saque! \n");
                     return false;
                 }
+
+            }
+
+            //TRANSFERIR
+            public void Transferencia(double valor, ContaCorrente origem, ContaCorrente destino)
+            {
+
+                if (this.saldo > valor)
+                {
+                    this.saldo -= valor;
+                    destino.saldo += valor;
+
+                    Console.WriteLine("\n-----TRANSFERÊNCIA-----");
+                    Console.WriteLine("\nTranferência feita no valor de: R${0}", valor);
+                    Console.WriteLine("\nSaldo atual da conta que tranferiu: R${0}", saldo);
+                    Console.WriteLine("\nSaldo atual da conta que recebeu a tranferência: R${0}", destino.saldo);
+                }
+
 
             }
 
